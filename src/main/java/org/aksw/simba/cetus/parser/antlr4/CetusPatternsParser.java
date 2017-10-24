64864c5514a02915b6e36662a63cecb441844f45
Micha <michael.roeder@uni-paderborn.de>
2017-10-24 17:34:06 +0200

    Updated the license information.

--- src/main/java/org/aksw/simba/cetus/parser/antlr4/CetusPatternsParser.java
+++ src/main/java/org/aksw/simba/cetus/parser/antlr4/CetusPatternsParser.java
@@ -1,3 +1,20 @@
+/**
+ * Cetus - Cetus performs Entity Typing Using patternS
+ * Copyright © 2015 Data Science Group (DICE) (michael.roeder@uni-paderborn.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 // Generated from CetusPatterns.g4 by ANTLR 4.5
 package org.aksw.simba.cetus.parser.antlr4;
 import org.antlr.v4.runtime.atn.*;
